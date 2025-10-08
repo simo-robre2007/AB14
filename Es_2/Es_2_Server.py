9ea0ab18a0ddeac6bd8d34ac61193d0b346301f7
simo-robre2007 <simone.robresco@itiscuneo.eu>
2025-10-08 09:50:37 +0200

    Update Es_2_Server.py

--- Es_2/Es_2_Server.py
+++ Es_2/Es_2_Server.py
@@ -1,1 +1,35 @@
+from movimenti import AlphaBot
 
+import socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)   # AF_INET = ipv4
+                                                                    # SOCK_DGRAM = udp
+
+server_socket.bind(("0.0.0.0", 7000))
+robot.Alphabot
+
+while True:
+    msg, address = server_socket.recvfrom(1024)
+    print(msg.decode())
+    if (msg == 'w')
+        robot.forward(2)
+        A = 'Avanti'
+        server_socket.sendto(A.encode(), address)
+
+    if (msg == 's')
+          robot.backward(2)
+          I = 'Indietro'
+          server_socket.sendto(I.encode(), address)
+
+    if (msg == 'a')
+          robot.left(2)
+          S = 'Sinistra'
+          server_socket.sendto(S.encode(), address)
+
+    if (msg == 'd')
+          robot.right(2)
+          D = 'Destra'
+          server_socket.sendto(D.encode(), address)
+
+    if (msg == 'EXIT')
+        robot.stop()
+    
