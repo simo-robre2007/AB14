f6b079bdee88685085e2af50aae62e5a8aa372c4
simo-robre2007 <simone.robresco@itiscuneo.eu>
2025-10-08 09:52:31 +0200

    Update Es_2_Server.py

--- Es_2/Es_2_Server.py
+++ Es_2/Es_2_Server.py
@@ -1,8 +1,7 @@
 from movimenti import AlphaBot
 
 import socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)   # AF_INET = ipv4
-                                                                    # SOCK_DGRAM = udp
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 server_socket.bind(("0.0.0.0", 7000))
 robot.Alphabot
